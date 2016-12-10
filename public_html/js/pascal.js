ad3a3f105faba80a8b84e4b823b2e6783b323464
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2016-12-10 22:38:10 +0100

    init

---
+++ public_html/js/pascal.js
@@ -0,0 +1,27 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @param {string} lineId 
+ * @param {string} targetId
+ * */
+var printPascal = function (lineId, targetId){
+	// TODO....
+};
+
+/**
+ * @param {integer} numOfLine 
+ * @returns {array} a 2-Dimension Array, represents a Pascal Triangle.
+ * */
+var computePascalTriangel = function(numOfLine){
+	//
+	// return an array of array
+};
+
+
+
+
+
